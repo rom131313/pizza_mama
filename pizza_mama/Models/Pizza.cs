7343ecedce8a08660a9bd24dc22d1362cf859765
halison andrianaivoarivelo <halison.andria@gmail.com>
2025-02-16 19:03:06 +0100

    229. Exercice : Modifications de l’interface d’administration

--- pizza_mama/Models/Pizza.cs
+++ pizza_mama/Models/Pizza.cs
@@ -4,11 +4,14 @@ namespace pizza_mama.Models
 {
     public class Pizza
     {
-        public int PizzaId { get; set; } 
+        public int PizzaId { get; set; }
+        [Display(Name = "Nom")]
         public string nom { get; set; }
+        [Display(Name = "Prix (€)")]
         public float prix {get; set; }
         [Display(Name = "Végétarienne")]
         public bool vegetarienne { get; set; }
+        [Display(Name = "Ingrédients")]
         public string ingredients { get; set; }
     }
 
