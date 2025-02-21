59b848c633614e32b1fa80e783d9ed45d26c5f15
halison andrianaivoarivelo <halison.andria@gmail.com>
2025-02-21 18:12:09 +0100

    259. Exercice : Retourner toutes les pizzas

--- pizza_mama/Controllers/ApiController.cs
+++ pizza_mama/Controllers/ApiController.cs
@@ -1,25 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using pizza_mama.Data;
 using pizza_mama.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace pizza_mama.Controllers
 {
-    //[Route("api/[controller]")]
+ 
     [Route("[controller]")]
     [ApiController]
-    //public class ApiController : ControllerBase
     public class ApiController : Controller
     {
+        DataContext dataContext;
+        public ApiController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
 
         [HttpGet]
         [Route("GetPizzas")]
         public IActionResult GetPizzas()
         {
-            var pizza = new Pizza() {  nom="pizza test", prix=8, vegetarienne=false, ingredients="tomate, oignons"};
+            //var pizza = new Pizza() {  nom="pizza test", prix=8, vegetarienne=false, ingredients="tomate, oignons"};
 
-            //return new string[] { "Pizza", "value2" };
-            return Json(pizza);
+            // 1 - DataContext
+            //2 - Récupérer les pizzas -> Json
+
+            var pizzas = dataContext.Pizzas.ToList();
+            return Json(pizzas);
         }
 
 
