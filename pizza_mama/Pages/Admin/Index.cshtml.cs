f361e47c63084a95ee0cdc9798bc3c9bd3cb6a6b
halison andrianaivoarivelo <halison.andria@gmail.com>
2025-02-21 14:06:46 +0100

    240. Exercice : Afficher les Pizzas

--- pizza_mama/Pages/Admin/Index.cshtml.cs
+++ pizza_mama/Pages/Admin/Index.cshtml.cs
@@ -3,8 +3,8 @@ using Microsoft.AspNetCore.Authentication;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace pizza_mama.Pages.Admin
+
 {
     public class IndexModel : PageModel
     {
